28d03f4028f16517d9bf16933851b5dbf194ef80
lucasc84 <curtolbt@gmail.com>
2025-09-04 00:41:11 -0300

    primer ommit preentrega 2

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,4 +1,18 @@
+import { useState, useEffect, use } from 'react'
+import {getProductos} from '../mock/AsyncMockService'
+
 const ItemListContainer = ({mensaje}) => {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        getProductos()
+        .then((res) => console.log (res))
+
+    }, [])
+
+
+    console.log(' ItemListContainer' )
     return(
     <div>
         <h1>{mensaje}</h1>
---
+++ src/mock/AsyncMockService.jsx
@@ -0,0 +1,34 @@
+const productos = [
+  {
+    id: '1',
+    title: 'Short',
+    description: 'Short de entrenamiento resistente al cloro, pensado para sesiones frecuentes. Ajuste cómodo con soporte frontal y cintura segura. Secado rápido y tejido durable que mantiene forma y color. Ideal para piscina y trabajos de técnica.',
+    price: 100,
+    stock: 10,
+    category: 'hombre',
+    image: '/img/hombre-short.webp',
+  },
+    {
+    id: '2',
+    title: 'Jammer',
+    description: 'Jammer hidrodinámico para entrenamientos intensivos. Reduce la resistencia al avance y ofrece sujeción muscular suave. Tejido de secado rápido, alta resistencia al cloro y protección UV. Ajuste firme sin sacrificar comodidad ni movilidad.',
+    price: 100,
+    stock: 10,
+    category: 'hombre',
+    image: '/img/hombre-jammer.webp',
+  },
+];
+
+
+let error = false
+export const getProductos = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        if (error) {
+            reject('Error al obtener los productos')
+        } else {
+      resolve(productos)
+    }
+    }, 2000);
+  });
+}
