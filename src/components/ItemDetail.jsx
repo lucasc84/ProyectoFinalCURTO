6713e66199210668cb6f4c7cd5bf04d66e73ff0f
lucasc84 <curtolbt@gmail.com>
2025-09-21 18:48:44 -0300

    cambie html por cards de bootstrap en itemDetail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import ItemCount from './ItemCount'
-
+import Card from 'react-bootstrap/Card'
 const ItemDetail = ({ detalle }) => {
 
   const onAdd = (cantidad) => {
@@ -14,12 +14,23 @@ const ItemDetail = ({ detalle }) => {
 
   return (
     <div>
-<h2>Detalle de: {detalle?.name}</h2>
-<img src={detalle?.image} alt={detalle?.name}/>
+
+<Card bg = "info" border="primary" style={{ width: 'auto' }}>
+      <Card.Title>{detalle?.name}</Card.Title>
+      <Card.Body>
+        
+        <Card.Text>
+
 <p>{detalle?.description}</p>
 <p>Precio: ${detalle?.price}</p>
 <p>Stock disponible: {detalle?.stock}</p>
 <ItemCount stock={detalle?.stock} onAdd={onAdd}/>
+
+        </Card.Text>
+                <Card.Img variant="top" src={detalle?.image} alt = {detalle?.name}/>
+
+        </Card.Body>
+    </Card>
 
 
     </div>
