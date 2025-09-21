b5f8b2372f1dc083db639e04223c8212bad9c632
lucasc84 <curtolbt@gmail.com>
2025-09-21 19:01:01 -0300

    mejore estilos de itemDetail con grid de bootstrap

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -3,6 +3,8 @@
 import React from 'react'
 import ItemCount from './ItemCount'
 import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 const ItemDetail = ({ detalle }) => {
 
   const onAdd = (cantidad) => {
@@ -13,27 +15,25 @@ const ItemDetail = ({ detalle }) => {
 
 
   return (
-    <div>
-
-<Card bg = "info" border="primary"       className=" my-4 mx-4" 
-style={{ width: 'auto' }}>
-      <Card.Title>{detalle?.name}</Card.Title>
-      <Card.Body>
-        
-        <Card.Text>
-
-<p>{detalle?.description}</p>
-<p>Precio: ${detalle?.price}</p>
-<p>Stock disponible: {detalle?.stock}</p>
-<ItemCount stock={detalle?.stock} onAdd={onAdd}/>
-
-        </Card.Text>
-                <Card.Img variant="top" src={detalle?.image} alt = {detalle?.name}/>
-
+<div>
+      <Card bg="info" border="primary" className="mx-auto my-4 w-100" style={{ maxWidth: '900px' }}>
+        <Card.Body>
+          <Row>
+            <Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
+              <Card.Img variant="top" src={detalle?.image} alt={detalle?.name} style={{ maxHeight: '300px', width: 'auto' }}/>
+            </Col>
+            <Col xs={12} md={6}>
+              <Card.Title>{detalle?.name}</Card.Title>
+              <Card.Text>
+                <p>{detalle?.description}</p>
+                <p>Precio: ${detalle?.price}</p>
+                <p>Stock disponible: {detalle?.stock}</p>
+                <ItemCount stock={detalle?.stock} onAdd={onAdd}/>
+              </Card.Text>
+            </Col>
+          </Row>
         </Card.Body>
-    </Card>
-
-
+      </Card>
     </div>
   )
 }
